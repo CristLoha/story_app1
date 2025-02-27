dede631726598671da09365ab365a8cd1f5d945c
CristLoha <cristloha2205@gmail.com>
2025-02-27 14:37:05 +0800

    first commit

---
+++ lib/core/theme_manager/font_manager.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FontFamilyConstant {
+  static String fontFamily = GoogleFonts.quicksand.toString();
+}
+
+class FontWeightManager {
+  static FontWeight thin = FontWeight.w100;
+  static FontWeight extraLight = FontWeight.w200;
+  static FontWeight light = FontWeight.w300;
+  static FontWeight regular = FontWeight.w400;
+  static FontWeight medium = FontWeight.w500;
+  static FontWeight semiBold = FontWeight.w600;
+  static FontWeight bold = FontWeight.w700;
+  static FontWeight extraBold = FontWeight.w800;
+  static FontWeight black = FontWeight.w900;
+}
+
+class FontSizeManager {
+  static const double f12 = 12.0;
+  static const double f14 = 14.0;
+  static const double f16 = 16.0;
+  static const double f18 = 18.0;
+  static const double f20 = 20.0;
+  static const double f22 = 22.0;
+  static const double f24 = 24.0;
+  static const double f26 = 26.0;
+  static const double f28 = 28.0;
+}
