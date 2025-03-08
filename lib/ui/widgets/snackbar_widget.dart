c973fcedf2c0bbb166b8e38df66e07d840cb0cb3
CristLoha <cristloha2205@gmail.com>
2025-03-09 00:46:42 +0800

    update

--- lib/ui/widgets/snackbar_widget.dart
+++ lib/ui/widgets/snackbar_widget.dart
@@ -4,83 +4,107 @@ import 'package:story_app1/utils/theme_manager/color_manager.dart';
 import 'package:story_app1/utils/theme_manager/font_manager.dart';
 import 'package:story_app1/utils/theme_manager/style_manager.dart';
 
-class SnackbarWidget {
+class SnackbarWidget extends StatefulWidget {
+  final String message;
+
+  const SnackbarWidget({super.key, required this.message});
+
+  @override
+  State<SnackbarWidget> createState() => _SnackbarWidgetState();
+
   static void showError(BuildContext context, String message) {
     final overlay = Overlay.of(context);
-    final controller = AnimationController(
-      vsync: Navigator.of(context),
-      duration: const Duration(milliseconds: 300),
-    );
-    final animation = CurvedAnimation(
-      parent: controller,
-      curve: Curves.easeOut,
-    );
-
     final overlayEntry = OverlayEntry(
-      builder:
-          (context) => Positioned(
-            top: 60,
-            left: 20,
-            right: 20,
-            child: FadeTransition(
-              opacity: animation,
-              child: SlideTransition(
-                position: Tween<Offset>(
-                  begin: const Offset(0, -1),
-                  end: Offset.zero,
-                ).animate(animation),
-                child: Material(
-                  color: Colors.transparent,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 12,
-                    ),
-                    decoration: BoxDecoration(
-                      color: ColorsManager.secondary,
-                      borderRadius: BorderRadius.circular(12),
-                      boxShadow: [
-                        BoxShadow(
-                          color: ColorsManager.grey900,
-                          blurRadius: 5,
-                          offset: const Offset(0, 2),
-                        ),
-                      ],
-                    ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Lottie.asset(
-                          'assets/lottie/error_animation.json',
-                          width: 40,
-                          height: 40,
-                          fit: BoxFit.cover,
-                        ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: Text(
-                            message,
-                            style: getWhiteTextStyle().copyWith(
-                              fontSize: 16,
-                              fontWeight: FontWeightManager.semiBold,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
+      builder: (context) => SnackbarWidget(message: message),
     );
 
     overlay.insert(overlayEntry);
-    controller.forward();
-
-    Future.delayed(const Duration(seconds: 2), () async {
-      await controller.reverse();
+    Future.delayed(const Duration(seconds: 2), () {
       overlayEntry.remove();
     });
+  }
+}
+
+class _SnackbarWidgetState extends State<SnackbarWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeOut);
+
+    _controller.forward();
+    Future.delayed(const Duration(seconds: 2), () async {
+      await _controller.reverse();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 60,
+      left: 20,
+      right: 20,
+      child: FadeTransition(
+        opacity: _animation,
+        child: SlideTransition(
+          position: Tween<Offset>(
+            begin: const Offset(0, -1),
+            end: Offset.zero,
+          ).animate(_animation),
+          child: Material(
+            color: Colors.transparent,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              decoration: BoxDecoration(
+                color: ColorsManager.secondary,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: ColorsManager.grey900,
+                    blurRadius: 5,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Lottie.asset(
+                    'assets/lottie/error_animation.json',
+                    width: 40,
+                    height: 40,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Text(
+                      widget.message,
+                      style: getWhiteTextStyle().copyWith(
+                        fontSize: 16,
+                        fontWeight: FontWeightManager.semiBold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
