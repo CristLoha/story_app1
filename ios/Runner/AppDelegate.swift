b6ff6a3f9e7cef46acca62ab907089d4f011b262
CristLoha <cristloha2205@gmail.com>
2025-03-27 09:04:58 +0800

    Restore missing AppDelegate.swift and AndroidManifest.xml

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,17 @@
+import Flutter
+import UIKit
+import GoogleMaps  
+
+@main
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+if let apiKey = Bundle.main.object(forInfoDictionaryKey: "MAPS_API_KEY") as? String {
+    GMSServices.provideAPIKey(apiKey)
+}
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
